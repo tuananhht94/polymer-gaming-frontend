d4d3d73d6bdf7afa534af9f051a1ac188b5482e5
Anh Nguyen <tuananhht94@gmail.com>
2024-04-06 15:42:16 +0700

    Update config

---
+++ config/polymer.ts
@@ -0,0 +1,14 @@
+export const polymer = {
+  optimism: {
+    portAddr: '0x9070e98FE26CE56A36dd2226eD518c431F62D754',
+    channelId: 'channel-10',
+    timeout: 36000,
+  },
+  base: {
+    portAddr: '0x471B982D1A59D7A41794C700980a753049756Be3',
+    channelId: 'channel-11',
+    timeout: 36000,
+  },
+}
+
+export const polymerErc20Address = '0x930f82947cb4713cFB4498443A4dAE7BeC234287'
