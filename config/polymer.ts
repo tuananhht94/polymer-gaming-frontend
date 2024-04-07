fdfa12b56c9fa7aa60106247f6cd76f12b251a34
Anh Nguyen <tuananhht94@gmail.com>
2024-04-07 17:52:02 +0700

    Update polyer config

--- config/polymer.ts
+++ config/polymer.ts
@@ -3,15 +3,15 @@ export const polymer: {
   base: { portAddr: `0x${string}`; channelId: string; timeout: number }
 } = {
   optimism: {
-    portAddr: '0x79F583808a9E0cb170c4378430Efc2Da6cbBB58C',
+    portAddr: '0x04884758FD97bB9964Df3bAee2626182212faB34',
     channelId: 'channel-10',
     timeout: 36000,
   },
   base: {
-    portAddr: '0x0ADeaE20018A79f0702C689ffdBc3797e82D1158',
+    portAddr: '0x12cC44B928c37458f092F2298AfDfac2a41E4a34',
     channelId: 'channel-11',
     timeout: 36000,
   },
 }
 
-export const polymerErc20Address = '0x86AA51A17e398d84FF1e44c0821DA1da7b6A02c5'
+export const polymerErc20Address = '0x930f82947cb4713cFB4498443A4dAE7BeC234287'
